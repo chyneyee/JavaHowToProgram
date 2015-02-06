7dda3fe11681231a13715a1a2eaa63bc30b8ea69
chyneyee <windyspring@hotmail.com>
2015-02-06 14:01:34 +0800

    Fig. 2.1: Welcome1.java - Text-printing program.

---
+++ Chapter2/src/figures/Welcome1.java
@@ -0,0 +1,17 @@
+package figures;
+
+// Fig. 2.1: Welcome1.java
+// Text-printing program.
+
+public class Welcome1 {
+
+	// main method begins execution of Java application
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java Programming!");
+
+	} // end method main
+} // end class Welcome1
+
+/* Output:
+Welcome to Java Programming!
+ */
