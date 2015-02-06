c731082f6811c3b1b48a47c0007b155686151fb5
chyneyee <windyspring@hotmail.com>
2015-02-06 14:44:17 +0800

    Fig. 2.4: Welcome3.java - Printing multiple lines of text with a single statement.

---
+++ Chapter2/src/figures/Welcome3.java
@@ -0,0 +1,22 @@
+package figures;
+
+// Fig. 2.4: Welcome3.java
+// Printing multiple lines of text with a single statement.
+
+public class Welcome3 
+{
+	// main method begins execution of Java application
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome\nto\nJava\nProgramming!" );
+
+	}	// end method main
+	
+}	// end class Welcome3
+
+/* Output:
+Welcome
+to
+Java
+Programming!
+*/
