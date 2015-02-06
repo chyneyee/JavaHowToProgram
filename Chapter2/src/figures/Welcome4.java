bd278f96949c58de9fe257ddabf08464ffd2046e
chyneyee <windyspring@hotmail.com>
2015-02-06 14:50:53 +0800

    Fig. 2.6: Welcome4.java - Printing multiple lines in a dialog box.

---
+++ Chapter2/src/figures/Welcome4.java
@@ -0,0 +1,17 @@
+package figures;
+
+// Fig. 2.6: Welcome4.java
+// Printing multiple lines in a dialog box.
+public class Welcome4 
+{
+	// main method begins execution of Java application
+	public static void main(String[] args) {
+		System.out.printf("%s\n%s\n", "Welcome to", "Java Programming!");
+
+	} // end method main
+
+} // end class Welcome4
+
+/*
+ * Output: Welcome to Java Programming!
+ */
