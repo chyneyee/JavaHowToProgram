80d86f7219dd6f54007336119b6dcd931b36f10b
chyneyee <windyspring@hotmail.com>
2015-02-06 14:22:43 +0800

    Fig. 2.3: Welcome2.java - Printing a line of text with multiple statements.

---
+++ Chapter2/src/figures/Welcome2.java
@@ -0,0 +1,19 @@
+package figures;
+
+// Fig. 2.3: Welcome2.java
+// Printing a line of text with multiple statements.
+
+public class Welcome2 
+{
+	// main method begins execution of Java application
+	public static void main(String[] args) {
+		System.out.print("Welcome to ");
+		System.out.println("Java Programming!");
+
+	} // end method main
+
+} // end class Welcome2
+
+/* Output:
+Welcome to Java Programming!
+ */
