0b1207eb30f1e7975592195049d6be689ec0a569
chyneyee <windyspring@hotmail.com>
2015-03-02 21:36:33 +0800

    Fig. 3.17: Dialog1.java - Printing multiple lines in dialog box

---
+++ Chapter3/src/figures/Dialog1.java
@@ -0,0 +1,21 @@
+package figures;
+
+import javax.swing.JOptionPane; // import class JOptionPane
+
+/**
+ * @author mychin
+ * @date Mar 2, 2015
+ * @time 9:20:49 PM
+ * 
+ * Fig. 3.17: Dialog1.java
+ * Printing multiple lines in dialog box
+ */
+public class Dialog1 
+{
+	public static void main(String[] args) 
+	{
+		// display a dialog with a message
+		JOptionPane.showMessageDialog( null, "Welcome\nto\nJava" );
+	} // end main
+
+} // end class Dialog1
