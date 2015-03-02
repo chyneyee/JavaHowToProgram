1f41adb752e15faaadc2fed09e1cd74e2e3e74a8
chyneyee <windyspring@hotmail.com>
2015-03-02 18:12:21 +0800

    Fig. 3.4: GradeBook.java - Class declaration with a method that has a parameter.

---
+++ Chapter3/src/figures/GradeBook2.java
@@ -0,0 +1,19 @@
+package figures;
+
+/**
+ * @author mychin
+ * @date Mar 2, 2015
+ * @time 5:59:49 PM
+ * 
+ * Fig. 3.4: GradeBook.java 
+ * Class declaration with a method that has a parameter.
+ */
+public class GradeBook2 
+{
+	// display a welcome message to the GradeBook user
+	public void displayMessage( String courseName )
+	{
+		System.out.printf( "Welcome to the grade book for\n%s!\n", courseName);
+	} // end method displayMessage
+
+} // end class GradeBook2
