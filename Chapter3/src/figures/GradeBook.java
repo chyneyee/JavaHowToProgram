312991d3518485396802e62e11e79314448fc616
chyneyee <windyspring@hotmail.com>
2015-03-02 17:56:50 +0800

    Figure 3.1 - Class declaration with one method. Figure 3.2 - Create a GradeBook object and call its displayMessage method.

---
+++ Chapter3/src/figures/GradeBook.java
@@ -0,0 +1,19 @@
+package figures;
+
+/**
+ * @author mychin
+ * @date Mar 2, 2015
+ * @time 5:42:16 PM
+ * 
+ * Fig. 3.1: GradeBook.java
+ * Class declaration with one method.
+ */
+public class GradeBook 
+{
+	// display a welcome message to the GradeBook user
+	public void displayMessage()
+	{
+		System.out.println( "Welcome to the Grade Book!" );
+	} // end method displayMessage
+
+} // end class GradeBook
---
+++ Chapter3/src/figures/GradeBookTest.java
@@ -0,0 +1,28 @@
+package figures;
+
+/**
+ * @author mychin
+ * @date Mar 2, 2015
+ * @time 5:45:25 PM
+ * 
+ * Fig. 3.2: GradeBookTest.java
+ * Create a GradeBook object and call its displayMessage method.
+ */
+public class GradeBookTest 
+{
+	public static void main(String[] args) 
+	{
+		// create a GradeBook object and assign it to myGradeBook
+		GradeBook myGradeBook = new GradeBook();
+		
+		// call myGradeBook's displayMessage method
+		myGradeBook.displayMessage();
+	}	// end main
+
+}	// end class GradeBookTest
+
+
+
+/* Output:
+ * Welcome to the Grade Book!
+ */ 
